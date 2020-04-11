9a3ae208d521f0ef9f468e3d543c5c3fc22a5a69
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-11 04:46:57 -1200

    ADDED: Mandatory first part of email

---
+++ EmailValidation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+shopt -s extglob
+
+function checkEmailValidatiion
+{
+	if [[ $email =~ $emailPatternPart1  ]]
+	then
+   	echo "Valid"
+	else
+   	echo "Invalid"
+	fi
+}
+
+echo "Enter the Email"
+read email
+emailPatternPart1="^[a-z]{1}[a-z0-9]{63}$"
+checkEmailValidatiion
