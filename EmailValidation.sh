be56a9a4864640800d780d384c411ea56acdff9f
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-11 06:18:31 -1200

    MODIFIED: Put all pattern part of email together

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 
 shopt -s extglob
-
+#check email is valid or not
 function checkEmailValidatiion
 {
-	if [[ $email =~ $emailPatternPart4  ]]
+	if [[ $email =~ $emailPattern  ]]
 	then
-   	echo "Valid"
+		echo "Valid"
 	else
-   	echo "Invalid"
+		echo "Invalid"
 	fi
 }
 
@@ -18,5 +18,6 @@ emailPatternPart1="^[a-z]{1}[a-z0-9]{1,122}$"
 emailPatternPart2="^@[a-z0-9]{1,310}$"
 emailPatternPart3="^\.[a-z]{2,4}$"
 emailPatternPart4="^([.|_|+|-]?[a-z0-9]+)$"
-emailPatternPart5="^(\.[a-z]{2})?$"
+emailPatternPart5="^(\.[a-z]{2})?"
+emailPattern="^[a-z]{1}[a-z0-9]{1,132}([.|_|+|-]?[a-z0-9]{1,122}+)?@[a-z0-9]{1,310}(\.[a-z]{2,4})(\.[a-z]{2})?$"
 checkEmailValidatiion
